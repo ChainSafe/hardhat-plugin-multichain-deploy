793870402649276fcf2651d3e95e8a3a220f3ae9
BeroBurny <bero4net@gmail.com>
2024-01-18 16:26:07 +0100

    v0

--- packages/plugin/src/types.ts
+++ packages/plugin/src/types.ts
@@ -5,13 +5,31 @@ import {
   NonPayableCallOptions,
 } from "web3";
 
-export interface NetworkArguments<Abi extends ContractAbi = any> {
-  [network: string]: {
-    args: ContractConstructorArgs<Abi>;
-    initData?: string;
-  };
+/**
+ * NetworkArgument
+ * @property {boolean} args - Indicates whether the Courage component is present.
+ * @property {string} initData - Indicates whether the Power component is present.
+ */
+interface NetworkArgument<Abi extends ContractAbi = any> {
+  args: ContractConstructorArgs<Abi>;
+  initData?: string;
 }
 
+/**
+ * NetworkArguments
+ * @property {NetworkArgument} [network] - NetworkArgument
+ */
+export interface NetworkArguments<Abi extends ContractAbi = any> {
+  [network: string]: NetworkArgument<Abi>;
+}
+
+/**
+ * Options used for contract deployment
+ *
+ * @property {Uint8Array | string} [salt] - Indicates whether the Courage component is present.
+ * @property {Boolean} [isUniquePerChain] - Indicates whether the Power component is present.
+ * @property {NonPayableCallOptions} [customNonPayableTxOptions] - Indicates whether the Wisdom component is present.
+ */
 export interface DeployOptions {
   salt?: MatchPrimitiveType<"bytes32", unknown>;
   isUniquePerChain?: boolean;
