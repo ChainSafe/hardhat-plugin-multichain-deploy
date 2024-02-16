aba9b41d75fae93607ef88235d7122fe37214b52
BeroBurny <bero4net@gmail.com>
2024-02-16 10:36:38 +0100

    remove tests

--- example/hardhat.config.ts
+++ example/hardhat.config.ts
@@ -1,10 +1,9 @@
 import {HardhatUserConfig, vars} from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-web3-v4";
-import "@chainsafe/hardhat-ts-artifact-plugin";
-
-import "@chainsafe/hardhat-plugin-multichain-deploy";
 import {Environment} from "@buildwithsygma/sygma-sdk-core";
+
+import "@nomicfoundation/hardhat-toolbox";
+import "@chainsafe/hardhat-ts-artifact-plugin";
+import "@chainsafe/hardhat-plugin-multichain-deploy";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
